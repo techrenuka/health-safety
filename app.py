cb963ba3b2a122682298065fe041e4d7008aaf83
Mukesh Ashtekar <contact@techrenuka.com>
2025-02-01 13:56:26 +0530

    Update

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ print("Streamlit version:", st.__version__)
 print("PDFKit version:", pdfkit.__version__)
 print("Jinja2 version:", Environment.__module__)
 
-wkhtmltopdf = st.write(os.getcwd() + '/wkhtmltopdf/bin/wkhtmltoimage.exe' );
+fullpath = os.getcwd() + '/wkhtmltopdf/bin/wkhtmltoimage.exe'
+st.write(fullpath )
+
+wkhtmltopdf = fullpath;
 
 print(wkhtmltopdf)
 
